3391a58ad5775c6e03d5c2df5ae3550e9054dc4a
Richard Allen <Richard.Allen@ubisoft.com>
2024-10-18 11:31:34 +0100

    Blank project setup

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include <catch2/catch_session.hpp>
 
-int main()
+#include "servicelocatorpatternexamples.h"
+
+int main(const int argc, const char* const argv[])
 {
+    return Catch::Session().run(argc, argv);
 }
---
+++ servicelocatorpatternexamples.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Service Locator - Unit Tests")
+{
+}
